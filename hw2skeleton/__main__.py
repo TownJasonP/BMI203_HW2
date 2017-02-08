983235b6633b9a24f69e0e56acc9a0bcc234357f
TownJasonP <student@CCB2016-User1.local>
2017-02-07 23:49:41 -0800

    added final tests and some comparison metrics

--- hw2skeleton/__main__.py
+++ hw2skeleton/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 from .io import read_active_sites,write_clustering, write_mult_clusterings
 from .cluster import *#compute_jaccard_similarity,cluster_by_partitioning, cluster_hierarchically, initialize_k_clusters, update_clusters
 import matplotlib.pyplot as plt
@@ -35,8 +36,47 @@ if sys.argv[1][0:2] == '-R':
     write_clustering(sys.argv[3], clustering)
     #test_cluster_number(cluster_randomly, active_sites)
 
-plt.figure(facecolor = 'white')
+# compare clustering results
+def compute_overlap(a,b):
+    # ONLY VALID IF MULTIPLES ARE NOT EXPECTED WITHIN a OR WITHIN b!!!
+    count = 0
+    for i in a:
+        if i in b:
+            count = count + 1
+    return(count)
 
+def compare_cluster_overlap(n):
+    k_clusters = cluster_by_partitioning(active_sites, n)
+    h_clusters = cluster_hierarchically(active_sites, n)
+
+    permutations = list(itertools.permutations(range(n)))
+
+    overlaps = []
+    for i in permutations:
+        perm_overlap = []
+        for j in range(n):
+            perm_overlap.append(compute_overlap(k_clusters[j],h_clusters[i[j]]))
+        overlaps.append(np.sum(perm_overlap))
+    return float(max(overlaps))
+
+plt.figure(facecolor = 'white')
+x = []
+y = []
+for j in range(5): # number sims to run
+    print('Simulation # ' + str(j+1) + ' of 5')
+    for i in range(2,10): # range of cluster numbers to test
+        x.append(i)
+        y.append(compare_cluster_overlap(i)/len(active_sites))
+
+plt.ylabel('Overlap of Cluster Contents')
+plt.xlabel('Number of Clusters')
+plt.scatter(x,y, marker = 'o', color = 'blue', alpha = 0.4)
+plt.ylim(0,1)
+plt.xlim(0,10)
+plt.show()
+
+# Compare clustering quality across methods and across cluster number
+plt.figure(facecolor = 'white')
 for i in [cluster_hierarchically, cluster_by_partitioning, cluster_randomly]:
     methods = {'cluster_randomly':'Random', 'cluster_hierarchically':'Hierarchical', 'cluster_by_partitioning':'Partition'}
     color_dict = {'cluster_randomly':'green', 'cluster_hierarchically':'red', 'cluster_by_partitioning':'blue'}
--- test/test_cluster.py
+++ test/test_cluster.py
@@ -4,11 +4,25 @@ import os
 
 def test_similarity():
 
-    activesite_a = ['A','B','C']
-    activesite_b = ['A','B','D']
+    pdb_ids = [276, 4629, 10701]
+
+    active_sites = []
+    for id in pdb_ids:
+        filepath = os.path.join("data", "%i.pdb"%id)
+        active_sites.append(io.read_active_site(filepath))
 
     # update this assertion
-    assert cluster.compute_jaccard_similarity(activesite_a, activesite_b) == 0.5
+    assert cluster.compute_jaccard_similarity(active_sites[0], active_sites[1]) == 0.125
+    assert cluster.compute_jaccard_similarity(active_sites[1], active_sites[2]) == 3.0/7
+    assert cluster.compute_jaccard_similarity(active_sites[0], active_sites[2]) == 0.25
+
+    # verify distance metric properties:
+    #self similarity = 1
+    assert cluster.compute_jaccard_similarity(active_sites[0], active_sites[0]) == 1
+    # sim(a,b) = sim(b,a)
+    assert cluster.compute_jaccard_similarity(active_sites[0], active_sites[1]) == cluster.compute_jaccard_similarity(active_sites[1], active_sites[0])
+    # triangle inequality
+    assert cluster.compute_jaccard_similarity(active_sites[0], active_sites[1]) + cluster.compute_jaccard_similarity(active_sites[1], active_sites[2]) >= cluster.compute_jaccard_similarity(active_sites[0], active_sites[2])
 
 def test_partition_clustering():
     # tractable subset
@@ -19,8 +33,12 @@ def test_partition_clustering():
         filepath = os.path.join("data", "%i.pdb"%id)
         active_sites.append(io.read_active_site(filepath))
 
-    # update this assertion
-    assert cluster.cluster_by_partitioning(active_sites) == []
+    # verify cluster number
+    for i in [2,3]:
+        assert len(cluster.cluster_by_partitioning(active_sites,i)) == i
+
+    # had a lot of trouble pinning down how to test this adequately, ran out of time
+    assert (cluster.cluster_by_partitioning(active_sites,2))[0] == [active_sites[0]] or [active_sites[1],active_sites[2]]
 
 def test_hierarchical_clustering():
     # tractable subset
@@ -32,4 +50,4 @@ def test_hierarchical_clustering():
         active_sites.append(io.read_active_site(filepath))
 
     # update this assertion
-    assert cluster.cluster_hierarchically(active_sites) == []
+    assert cluster.cluster_hierarchically(active_sites,2) == [[active_sites[0]],[active_sites[1],active_sites[2]]]
